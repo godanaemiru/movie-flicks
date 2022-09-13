e9c0386fba4add14d4a91bfe704421996cb75c01
Godana <godanaemiru@gmail.com>
2022-09-13 20:49:16 +0300

    Added all the styles, search functionality and Api

--- script.js
+++ script.js
@@ -1,19 +1,70 @@
 const APIURL= 'https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=fc7f4571c91d868b56d9486debec1966&page=1';
 
 const IMPGPATH = 'https://image.tmdb.org/t/p/w1280';
+const SEARCHAPI = 'https://api.themoviedb.org/3/search/movie?&api_key=fc7f4571c91d868b56d9486debec1966&query=';
 
-async function getMovies() {
-    const resp = await fetch(APIURL);
+const main = document.getElementById('main');
+const form = document.getElementById('form');
+const search = document.getElementById('search');
+
+//Initially get favourite movies
+
+getMovies(APIURL);
+
+async function getMovies(url) {
+    const resp = await fetch(url);
     const respData = await resp.json();
 
 
-    console.log(respData);
-
-    
-
-    return respData;
+    showMovies(respData.results)
 }
 
-getMovies();
+function showMovies(movies) {
 
+    main.innerHTML = '';
+
+    movies.forEach(movie => {
+        const { poster_path, title, vote_average, overview} = movie;
+
+        const movieEl = document.createElement('div');
+        movieEl.classList.add('movie');
+
+        movieEl.innerHTML = `
+        <img src="${IMPGPATH + poster_path}" alt="${title}">
+        <div class="movie-info">
+            <h3>${title}</h3>
+            <span class="${getClassByRate(vote_average)}">${vote_average}</span>
+        </div> 
+        <div class="overview">
+            <h4>Overview:</h4>
+            ${overview}
+        </div>
+        `;
+
+        main.appendChild(movieEl);
+    });
+
+}
+
+function getClassByRate(vote) {
+    if(vote >= 8) {
+        return 'green';
+    } else if (vote >=5) {
+        return 'orange';
+    } else {
+        return 'red';
+    }
+}
+
+form.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const searchTerm = search.value; 
+
+    if(searchTerm) {
+        getMovies(SEARCHAPI + searchTerm);
+        search.value = '';
+    }
+
+});
 
